f0c01dcdb465f1e3449ac9f5f7bc6a90247005f0
Óscar Estay <oaestay@uc.cl>
2016-08-04 19:05:45 -0400

    Fixed capristano config

--- Capfile
+++ Capfile
@@ -26,7 +26,7 @@ require 'capistrano/rbenv'
 # require 'capistrano/rails/migrations'
 
 set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_ruby, 'ruby 2.3.1p112'
+set :rbenv_ruby, '2.3.1'
 
 # Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
 Dir.glob('lib/capistrano/tasks/*.cap').each { |r| import r }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,17 +6,13 @@ set :stage, :production
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
-
 # Extended Server Syntax
 # ======================
 # This can be used to drop a more detailed server
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'http://138.68.29.60/', user: 'deploy', roles: %w{web app}
+server '138.68.29.60', user: 'deploy', roles: %w{web app}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
