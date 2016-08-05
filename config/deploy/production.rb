330c72bdcd40ee87d227439bb10bcc4e80357433
Óscar Estay <oaestay@uc.cl>
2016-08-04 20:27:02 -0400

    Added db role to production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -12,7 +12,7 @@ set :stage, :production
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '138.68.29.60', user: 'deploy', roles: %w{web app}
+server '138.68.29.60', user: 'deploy', roles: %w{web app db}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
