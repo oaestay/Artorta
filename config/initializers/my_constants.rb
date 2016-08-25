ea8719e9edabaf6c9519c380b2acf9dbd8538d7a
Óscar Estay <oaestay@uc.cl>
2016-08-24 23:50:42 -0300

    Added another missing comma in my_constants.rb

--- config/initializers/my_constants.rb
+++ config/initializers/my_constants.rb
@@ -1,6 +1,6 @@
 require 'ostruct'
 
-STOP_WORDS = ['', 'un', 'una', 'unas', 'unos', 'uno', 'sobre', 'de', 'a', 'o'
+STOP_WORDS = ['', 'un', 'una', 'unas', 'unos', 'uno', 'sobre', 'de', 'a', 'o',
   'todo', 'también', 'tras', 'otro', 'algún', 'alguno', 'alguna', 'algunos',
   'algunas', 'es', 'eres', 'esta', 'estan', 'como', 'en', 'para', 'atras',
   'ambos', 'pero', 'por', 'cada', 'tiene', 'tienen', 'el', 'la', 'lo', 'las',
