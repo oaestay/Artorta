a819b78071fc48d0f823cdd4b87ee554118eea71
Óscar Estay <oaestay@uc.cl>
2016-08-24 23:48:15 -0300

    Added missing comma in my_constants.rb

--- config/initializers/my_constants.rb
+++ config/initializers/my_constants.rb
@@ -4,7 +4,7 @@ STOP_WORDS = ['', 'un', 'una', 'unas', 'unos', 'uno', 'sobre', 'de', 'a', 'o'
   'todo', 'también', 'tras', 'otro', 'algún', 'alguno', 'alguna', 'algunos',
   'algunas', 'es', 'eres', 'esta', 'estan', 'como', 'en', 'para', 'atras',
   'ambos', 'pero', 'por', 'cada', 'tiene', 'tienen', 'el', 'la', 'lo', 'las',
-  'los', 'su', 'si', 'dentro', 'solo', 'ultimo', 'largo', 'e', 'i', 'u', 'y'
+  'los', 'su', 'si', 'dentro', 'solo', 'ultimo', 'largo', 'e', 'i', 'u', 'y',
   'bastante', 'muchos', 'sus', 'dos', 'bajo', 'arriba', 'encima', 'muy', 'bien',
   'con', 'entre', 'sin', 'de', 'del', 'mi']
 
