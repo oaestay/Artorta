5f5a17ff669df334145ac3a574744758d87b9af4
oaestay <oaestay@uc.cl>
2020-04-20 02:54:05 -0400

    tmp: fix migration

--- db/migrate/20200420062005_add_priority_to_category.rb
+++ db/migrate/20200420062005_add_priority_to_category.rb
@@ -2,7 +2,7 @@ class AddPriorityToCategory < ActiveRecord::Migration[5.2]
   def change
     add_column :categories, :priority, :integer
     ["decoradas", "novios", "tradicionales", "cupcakes", "coctel", "postres"].each_with_index do |slug, index|
-      Category.find_by(slug: slug).update! priority: index
+      Category.find_by(slug: slug)&.update! priority: index
     end
   end
 end
